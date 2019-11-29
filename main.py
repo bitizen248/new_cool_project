82e2bc24a1309693e34233b704d437e877f4f0dc
Ivan Dmitriev <van4011@yandex.ru>
2019-11-29 18:43:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from pow import my_pow
+
+print("Очень крутой калькулятор!")
+a = input("Введите а =")
+b = input("Введите b =")
+
+# TODO высти операторы
+print("pow, возведение a в степень b")
+
+operator = input("Оператор =")
+c = None  # реузльтат сюда
+
+# TODO вычисления резултата
+if operator == "pow":
+    c = my_pow(a, b)
---
+++ pow.py
@@ -0,0 +1,2 @@
+def my_pow(a, b):
+    return a ** b
